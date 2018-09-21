a368b495013feadb0d9a5163aab5e6fd38e80afc
Vikas <potlurivikas@gmail.com>
2018-09-20 22:39:37 -0500

    :fire: Remove unused assets

--- gulpfile.js
+++ gulpfile.js
@@ -12,10 +12,8 @@ const buildCSS = require(`${GULP_TASK_FOLDER}/build-css`);
 const buildJS = require(`${GULP_TASK_FOLDER}/build-js`);
 const compileFiles = require(`${GULP_TASK_FOLDER}/compile-files`);
 const copyAssets = require(`${GULP_TASK_FOLDER}/copy-assets`);
-const stripPackage = require(`${GULP_TASK_FOLDER}/strip-package`);
 const buildForDist = require(`${GULP_TASK_FOLDER}/dist`);
 const zipDist = require(`${GULP_TASK_FOLDER}/zip`);
-const syncDist = require(`${GULP_TASK_FOLDER}/sync`);
 const watch = require(`${GULP_TASK_FOLDER}/watch`);
 
 gulp.on('err', swallowError);
@@ -26,13 +24,11 @@ gulp.task('css', cb => {buildCSS(cb);});
 gulp.task('js', () => buildJS());
 gulp.task('compile-files', cb => {compileFiles(cb);});
 gulp.task('copy-assets', () => copyAssets());
-gulp.task('strip-package', () => stripPackage());
 gulp.task('dist', cb => {buildForDist(cb);});
 gulp.task('zip', cb => {zipDist(cb);});
 gulp.task('default', () => run('watch'));
-gulp.task('sync', cb => {syncDist(cb);});
 gulp.task('watch', () => watch());
 
 gulp.task('build', cb => {
-	run('clean-build', 'sass', ['css', 'js'], 'copy-assets', 'compile-files', 'strip-package', cb);
+	run('clean-build', 'sass', ['css', 'js'], 'copy-assets', 'compile-files', cb);
 });
